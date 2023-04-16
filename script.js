72c24bec6e9a2beb275f18ba235abf8f1539215d
VincONEent <ducatez.vincent@gmail.com>
2023-04-16 17:11:22 +0200

    Edge the project toward completion

--- script.js
+++ script.js
@@ -6,51 +6,47 @@ let user_choice = 0 ;
 let container_width = 960 ;
 let container_height = 960 ;
 
-
 // for the purpose of starting the project, let's SET a value to user_choice
 user_choice = 16;
 
 
-//The number of case the user chose is only what is gonna appear in a lign or a column
-//to get the full number of cases in the grid, you need to put user choice to the square
 nbr_case = user_choice*user_choice;
 
 const container = document.querySelector('#container');
 let height = 0;
 let width = 0 ;
-//Calcul of the size of a div given the choice of the size of the grid
-height= container_height / user_choice;
-width =container_width / user_choice;
 
-for(let i=0 ; i<nbr_case ; i++ )
+
+function setGrid()
 {
-    //Creation of a new div
-    const newDiv = document.createElement('div');
-    //Setting the correct size to the div
-    newDiv.style.height = height.toString() + "px" ;
-    newDiv.style.width =  width.toString() + "px" ;
-    newDiv.classList.add('miniDiv');
-    //temporary tests
-    newDiv.textContent ="abc";
-    if((i%2 )===0)
+    //The number of case the user chose is only what is gonna appear in a lign or a column
+    //to get the full number of cases in the grid, you need to put user choice to the square
+    nbr_case = user_choice*user_choice;
+    //Calcul of the size of a div given the choice of the size of the grid
+    height= container_height / user_choice;
+    width =container_width / user_choice;
+    for(let i=0 ; i<nbr_case ; i++ )
     {
-        newDiv.style.backgroundColor="purple";
-    }
-    else
-        newDiv.style.backgroundColor="green";
+        //Creation of a new div
+        const newDiv = document.createElement('div');
+        //Setting the correct size to the div
+        newDiv.style.height = height.toString() + "px" ;
+        newDiv.style.width =  width.toString() + "px" ;
+        newDiv.classList.add('miniDiv');
     
-    //adding the div to the container
-    container.appendChild(newDiv);
-
+        //adding the div to the container
+        container.appendChild(newDiv);
+    }
+    for(let i=0 ; i<nbr_case ; i++)
+    {
+        document.getElementsByClassName('miniDiv')[i].addEventListener('mouseover', (e)  => change(i));
+    }
 }
-/*
-document.getElementsByClassName('miniDiv')[1].addEventListener('click', (e)=> console.log("That big line of code worked"));
-document.getElementsByClassName('miniDIv').addEventListener('click', (e)=> console.log("That big line of code worked"));
-*/
 
+setGrid();
 //Connect every miniDiv to a function
-for(let i=0 ; i<nbr_case ; i++)
-{
-    document.getElementsByClassName('miniDiv')[i].addEventListener('click', (e)  => console.log("That big line of code worked"));
-}
 
+
+function change(index){
+    document.getElementsByClassName('miniDiv')[index].style.backgroundColor="black";
+}
